281c51e98f623b5ed2f274ab58eadac229d722c1
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-03-23 03:11:28 +0545

    fixed login vercel issue

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,10 +3,15 @@ import { NextResponse } from "next/server";
 import { loginUser } from "@/modules/auth/auth.service";
 import { connectDB } from "@/lib/db";
 
+interface LoginPayload {
+  email: string;
+  password: string;
+}
+
 export async function POST(req: Request) {
   await connectDB();
 
-  const { email, password } = await req.json();
+  const { email, password } = (await req.json()) as LoginPayload;
 
   try {
     const { user, token } = await loginUser(email, password);
@@ -16,17 +21,25 @@ export async function POST(req: Request) {
       user,
     });
 
+    // Set cookie for production only with secure
     res.cookies.set("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
       path: "/",
+      maxAge: 60 * 60 * 24 * 7, // optional: 7 days
     });
 
     return res;
-  } catch (err: any) {
+  } catch (err: unknown) {
+    let message = "Something went wrong";
+
+    if (err instanceof Error) {
+      message = err.message;
+    }
+
     return NextResponse.json(
-      { success: false, message: err.message },
+      { success: false, message },
       { status: 401 }
     );
   }
