0854bc9cebcd135b8384b0364179843eb37859b8
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-03-31 23:36:52 +0545

    update layout and css

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -60,16 +60,17 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
           marginLeft: isDesktop ? (isCollapsed ? "80px" : "256px") : "0px"
         }}
         transition={{ type: "spring", stiffness: 300, damping: 30 }}
-        className="flex-1 flex flex-col min-w-0 w-full"
+        className="flex-1 flex flex-col min-w-0 w-full overflow-hidden"
       >
         <Header
           isCollapsed={isCollapsed}
           onMenuClick={() => setIsMobileMenuOpen(true)}
         />
         <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
-          <div className="max-w-full mx-auto space-y-6 sm:space-y-8">
-            {children}
-          </div>
+          {/* <div className="max-w-full mx-auto space-y-6 sm:space-y-8 h-screen"> */}
+          {children}
+          {/* </div> */}
+          <div className="h-screen"></div>
         </main>
       </motion.div>
     </div>
